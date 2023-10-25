2a7732c5c1d89e5d42b4f7367f5b411718dedfe5
yunjung <yunjj21@hs.ac.kr>
2023-10-25 18:02:27 +0900

    MainActivity BackEvent 설정

--- app/src/main/java/com/yunjung/runningday/presentation/main/MainActivity.kt
+++ app/src/main/java/com/yunjung/runningday/presentation/main/MainActivity.kt
@@ -22,5 +22,42 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
+
+        setBackEvent()
+    }
+
+    /*
+    * backEvent 설정
+    * */
+    private fun setBackEvent(){
+        val onBackPressedDispatcher = object: OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                if(System.currentTimeMillis() - backPressedTime < 2000){
+                    exitApp()
+                }else {
+                    showAppExitToast()
+                }
+            }
+        }
+        this.onBackPressedDispatcher.addCallback(this, onBackPressedDispatcher)
+    }
+
+    private var backPressedTime = 0L
+    private fun showAppExitToast(){
+        backPressedTime = System.currentTimeMillis()
+        Toast.makeText(this, "\'뒤로\'버튼을 한번 더 누르면 종료됩니다.",
+            Toast.LENGTH_SHORT).show()
+    }
+
+    /*
+    * 앱 종료
+    * */
+    private fun exitApp(){
+        CoroutineScope(Dispatchers.Default).launch {
+            delay(200)
+            withContext(Dispatchers.Main){
+                finishAndRemoveTask()
+            }
+        }
     }
 }
